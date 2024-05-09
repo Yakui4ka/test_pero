a6ef99e63a959a505a679640a09d756a4ab0ab3e
Yakui4ka <97041498+Yakui4ka@users.noreply.github.com>
2024-05-09 13:54:03 +0300

    Update ProductPageTest.java

--- src/test/java/com/example/demo/ProductPageTest.java
+++ src/test/java/com/example/demo/ProductPageTest.java
@@ -1,25 +1,38 @@
 package com.example.demo;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.junit.jupiter.api.*;
 
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ProductPageTest {
+
     @BeforeAll
     public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
+        Configuration.browserSize = "1920x1080";
     }
 
     @BeforeEach
     public void setUp() {
         Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("https://vk.com/club225299895?w=product-225299895_10044406");
     }
 
     @Test
-    public void search() throws InterruptedException {
-        Thread.sleep(1_000);
+    public void verifyProductDetails() {
+        open("https://vk.com/club225299895?w=product-225299895_10044406");
+        $(byText("Услуги")).should(Condition.exist);
+
+    }
+
+    @Test
+    public void verifyCommunityHomePage() {
+        open("https://vk.com/club225299895");
+        $(byText("Подробная информация")).should(Condition.exist);
+
     }
 }
